0a40f0bb6a8942f83fdcf97d8baa03906f346ded
sasha <sashapozhidaev07@example.com>
2023-05-14 15:25:45 +0600

    routes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::namespace('App\Http\Controllers')->middleware('verified')->group(function () {
+Route::namespace('App\Http\Controllers')->group(function () {
     Route::get('/', 'MainController@index')->name('index');
     Route::get('/search', 'SearchController@search')->name('search');
 
